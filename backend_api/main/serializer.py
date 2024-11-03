5b987245a39ea0880bc5ab4b7c096cc404f8e3d4
Tooni Nakka <tooninakka6@gmail.com>
2024-11-03 14:18:17 +0000

    first commit

---
+++ backend_api/main/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from . import models
+
+class VendorSerializer(serializers.Serializer):
+    class Meta:
+        model = models.Vendor
+        fields = ['user', 'address', ]
---
+++ backend_api/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from . import serializer
+from . import models
+from rest_framework import generics
+# Create your views here.
+class VendorList(generics.ListAPIView):
+    queryset = models.Vendor.objects.all()
+    print(queryset)
+    serializer_class = serializer.VendorSerializer
